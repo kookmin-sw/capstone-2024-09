13ca8e9ae80e99896a3317893f6fd764aab07ecd
ISCMSHY <sungwhan4854@naver.com>
2024-04-16 10:41:46 +0900

    Set react flask 연동

--- capstone_design/flask_app/app/app.py
+++ capstone_design/flask_app/app/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'hello world'
 
+@app.route('/users')
+def users():
+	# users 데이터를 Json 형식으로 반환한다
+    return {"members": ["M1", "M2", "M3"]}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
