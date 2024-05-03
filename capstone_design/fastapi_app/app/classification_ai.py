28cf4e6273926f46383442877f007d79b30e4772
ISCMSHY <sungwhan4854@naver.com>
2024-05-03 18:41:39 +0900

    Set classification ai model import

--- capstone_design/fastapi_app/app/classification_ai.py
+++ capstone_design/fastapi_app/app/classification_ai.py
@@ -1,15 +1,14 @@
 import joblib
 from konlpy.tag import Okt
 
-mlp_model = joblib.load('./mlp_model.joblib')
+mlp_model = joblib.load('./AI/mlp_model.joblib')
+vectorizer = joblib.load('./AI/tfidfvectorizer.joblib')
 okt = Okt()
 
 conversation_X = []
 sentence = "저는 축구 선수가 되고 싶습니다. 어떻게 하면 되는지 알려주세요."
 
 conversation_X.append(" ".join(okt.nouns(sentence)))
-
-vectorizer = joblib.load('tfidfvectorizer.joblib')
 X_tfidf = vectorizer.transform(conversation_X).toarray()
 
 predictions = mlp_model.predict(X_tfidf)
