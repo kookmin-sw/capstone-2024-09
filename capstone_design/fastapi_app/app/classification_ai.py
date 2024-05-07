4b744e8c6bfba3536b010b30215b11cb84aba5b8
ISCMSHY <sungwhan4854@naver.com>
2024-05-07 13:55:48 +0900

    Set classification model test

--- capstone_design/fastapi_app/app/classification_ai.py
+++ capstone_design/fastapi_app/app/classification_ai.py
@@ -8,7 +8,8 @@ okt = Okt()
 def predict(sentence):
     conversation_X = []
     conversation_X.append(" ".join(okt.nouns(sentence)))
-    X_tfidf = vectorizer.transform(conversation_X).toarray()
+    # X_tfidf = vectorizer.transform(conversation_X).toarray()
 
-    predictions = mlp_model.predict(X_tfidf)
-    return predictions
+    # predictions = mlp_model.predict(X_tfidf)
+    # return predictions
+    return 0
