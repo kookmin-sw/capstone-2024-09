f12d1aac0d322daf13e03bc4c70b51cc3550f0e9
ISCMSHY <sungwhan4854@naver.com>
2024-05-07 10:19:26 +0900

    Set classification model test

--- capstone_design/fastapi_app/app/classification_ai.py
+++ capstone_design/fastapi_app/app/classification_ai.py
@@ -1,14 +1,14 @@
 import joblib
 from konlpy.tag import Okt
 
-# mlp_model = joblib.load('./mlp_model.joblib')
-# vectorizer = joblib.load('./tfidfvectorizer.joblib')
-# okt = Okt()
-#
-# def predict(sentence):
-#     conversation_X = []
-#     conversation_X.append(" ".join(okt.nouns(sentence)))
-#     X_tfidf = vectorizer.transform(conversation_X).toarray()
-#
-#     predictions = mlp_model.predict(X_tfidf)
-#     return predictions
+mlp_model = joblib.load('/app/mlp_model.joblib')
+vectorizer = joblib.load('/app/tfidfvectorizer.joblib')
+okt = Okt()
+
+def predict(sentence):
+    conversation_X = []
+    conversation_X.append(" ".join(okt.nouns(sentence)))
+    X_tfidf = vectorizer.transform(conversation_X).toarray()
+
+    predictions = mlp_model.predict(X_tfidf)
+    return predictions
